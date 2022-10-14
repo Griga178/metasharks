1dfb70cfd38e33548c09a3cd298c4946c8021084
Griga178 <Grisha.tis@gmail.com>
2022-10-14 21:55:15 +0300

    django create models

---
+++ sharkstest/salesmanager/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import Color, CarBrand, CarModel, Order
+
+admin.site.register(Color)
+admin.site.register(CarBrand)
+admin.site.register(CarModel)
+admin.site.register(Order)
---
+++ sharkstest/salesmanager/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from datetime import datetime
+from django.utils import timezone
+
+# Create your models here.
+class Color(models.Model):
+    """
+    Возможные цвета автомобилей
+    """
+    name = models.CharField(max_length = 200, help_text = "Введите цвет автомобиля")
+
+    def __str__(self):
+        return self.name
+
+class CarBrand(models.Model):
+    """
+    Возможные марки автомобилей
+    """
+    name = models.CharField(max_length = 200, help_text = "Введите марку автомобиля")
+
+    def __str__(self):
+        return self.name
+
+class CarModel(models.Model):
+    """
+    Возможные модели автомобилей
+    """
+    name = models.CharField(max_length = 200, help_text = "Введите модель автомобиля")
+    brand_name = models.ForeignKey('CarBrand', null = False, on_delete = models.PROTECT)
+
+    def __str__(self):
+        return f'{self.brand_name.name} {self.name}'
+
+    class Meta:
+        ordering = ["brand_name"]
+
+class Order(models.Model):
+    """
+    Возможные цвета автомобилей
+    """
+    id = models.BigAutoField(primary_key = True)
+    color_name = models.ForeignKey('Color', null = False, on_delete = models.PROTECT)
+    model_name = models.ForeignKey('CarModel', null = False, on_delete = models.PROTECT)
+    ammount = models.IntegerField(help_text = "Введите колличество автомобилей")
+    order_date = models.DateField(default = timezone.now, null = False)
+
+    def __str__(self):
+        return f'{self.id}. {self.model_name.brand_name.name} {self.model_name.name} - {self.color_name.name}'
+    class Meta:
+        ordering = ["order_date"]
